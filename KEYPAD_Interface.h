5fe61b47b853bb26d3307622b513a27eb94aae85
bishoy <bishoyadel1@gmail.com>
2024-01-28 14:19:54 +0200

    ADDING LCD & KEYPAD

---
+++ KEYPAD_Interface.h
@@ -0,0 +1,16 @@
+/*
+ * KEYPAD_interface.h
+ *
+ *  Created on: jan 28, 2024
+ *      Author: Bishoy
+ */
+
+#ifndef INCLUDE_HAL_KEYPAD_KEYPAD_INTERFACE_H_
+#define INCLUDE_HAL_KEYPAD_KEYPAD_INTERFACE_H_
+
+
+
+u8 HKEYPAD_u8GetPressedKey(void);
+
+
+#endif /* INCLUDE_HAL_KEYPAD_KEYPAD_INTERFACE_H_ */
